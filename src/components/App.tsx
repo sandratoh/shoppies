c32b6ba000dd81b9f9fb5a6f5236fb3236bce38f
Sandra To <sandra.to.e@gmail.com>
2021-05-07 15:05:51 -0700

    Render alert message using implicit condition

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,21 @@
+// Libraries
+import { useContext } from 'react';
+
 // Components
 import Alert from './Alert';
 import NominationCard from './Nomination/NominationCard';
 import ResultCard from './Result/ResultCard';
 import SearchBar from './SearchBar';
 
+// Helpers
+import { MovieContext } from '../Provider/MovieDataProvider';
+
 // Stylesheet
 import './App.scss';
 
 export const App: React.FC<{}> = () => {
+  const { nominations } = useContext(MovieContext);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,7 +26,7 @@ export const App: React.FC<{}> = () => {
           <h3>Movie title:</h3>
           <SearchBar />
         </div>
-        <Alert />
+        {nominations.length >= 5 && <Alert />}
         <div className="App-cards">
           <ResultCard />
           <NominationCard />
