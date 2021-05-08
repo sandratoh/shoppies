f2c03e6bc915198c142d0b8828339abc177b7bbc
Sandra To <sandra.to.e@gmail.com>
2021-05-08 13:40:50 -0700

    Render alt string if nomination list is empty fixing .map crash

--- src/components/Confirm.tsx
+++ src/components/Confirm.tsx
@@ -18,7 +18,7 @@ import { MovieContext } from '../Provider/MovieDataProvider';
 import './Confirm.scss';
 
 export const Confirm = () => {
-  // const { nominations } = useContext(MovieContext);
+  const { nominations } = useContext(MovieContext);
   const [open, setOpen] = useState(false);
 
   const handleClickOpen = () => {
@@ -29,73 +29,73 @@ export const Confirm = () => {
     setOpen(false);
   };
 
-  const nominations = [
-    {
-      Title: 'Mean Girls',
-      Year: '2004',
-      imdbID: 'tt0377092',
-      Type: 'movie',
-      Poster:
-        'https://m.media-amazon.com/images/M/MV5BMjE1MDQ4MjI1OV5BMl5BanBnXkFtZTcwNzcwODAzMw@@._V1_SX300.jpg',
-    },
-    {
-      Title: 'The Avengers',
-      Year: '2012',
-      imdbID: 'tt0848228',
-      Type: 'movie',
-      Poster:
-        'https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg',
-    },
-    {
-      Title: 'The Matrix',
-      Year: '1999',
-      imdbID: 'tt0133093',
-      Type: 'movie',
-      Poster:
-        'https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg',
-    },
-  ];
+  // const nominations = [
+  //   {
+  //     Title: 'Mean Girls',
+  //     Year: '2004',
+  //     imdbID: 'tt0377092',
+  //     Type: 'movie',
+  //     Poster:
+  //       'https://m.media-amazon.com/images/M/MV5BMjE1MDQ4MjI1OV5BMl5BanBnXkFtZTcwNzcwODAzMw@@._V1_SX300.jpg',
+  //   },
+  //   {
+  //     Title: 'The Avengers',
+  //     Year: '2012',
+  //     imdbID: 'tt0848228',
+  //     Type: 'movie',
+  //     Poster:
+  //       'https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg',
+  //   },
+  //   {
+  //     Title: 'The Matrix',
+  //     Year: '1999',
+  //     imdbID: 'tt0133093',
+  //     Type: 'movie',
+  //     Poster:
+  //       'https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg',
+  //   },
+  // ];
 
-  const movieList = nominations.map(nomination => (
-    <li>{`${nomination.Title} (${nomination.Year})`}</li>
-  ));
+  const movieList = nominations
+    ? nominations.map(nomination => (
+        <li>{`${nomination.Title} (${nomination.Year})`}</li>
+      ))
+    : 'There are no movies in your nomination list.';
 
   return (
-    <section className="confirm-dialog">
-      <Dialog
-        open={true}
-        onClose={handleClose}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-        fullWidth
-        maxWidth="sm"
-      >
-        <DialogTitle id="alert-dialog-title">
-          {'Clear All from Nomination List?'}
-        </DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            {movieList}
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button
-            color="primary"
-            variant="text"
-            onClick={() => console.log('confirm button clicked')}
-          >
-            Confirm
-          </Button>
-          <Button
-            color="primary"
-            variant="text"
-            onClick={() => console.log('cancel button clicked')}
-          >
-            Cancel
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </section>
+    <Dialog
+      open={true}
+      onClose={handleClose}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+      fullWidth
+      maxWidth="sm"
+    >
+      <DialogTitle id="alert-dialog-title">
+        {'Clear All from Nomination List?'}
+      </DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-description">
+          {movieList}
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button
+          color="primary"
+          variant="text"
+          onClick={() => console.log('confirm button clicked')}
+        >
+          Confirm
+        </Button>
+        <Button
+          color="primary"
+          variant="text"
+          onClick={() => console.log('cancel button clicked')}
+        >
+          Cancel
+        </Button>
+      </DialogActions>
+    </Dialog>
   );
 };
 
