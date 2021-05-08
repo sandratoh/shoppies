ec8a1d774699cac2bcfd144c20a311607979145f
Sandra To <sandra.to.e@gmail.com>
2021-05-08 15:29:51 -0700

    Update theme color and list style

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,6 @@
 // Libraries
 import { useContext } from 'react';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
 // Components
 import Alert from './Alert';
@@ -14,29 +15,43 @@ import { AppContext } from '../Provider/DataProvider';
 // Stylesheet
 import './App.scss';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#004c3f',
+      dark: '#008060',
+    },
+    secondary: {
+      main: '#f44336;',
+    },
+  },
+});
+
 export const App: React.FC<{}> = () => {
   const { nominations } = useContext(AppContext);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>The Shoppies</h1>
-      </header>
-      <main>
-        <div className="App-search">
-          <h3>Movie title:</h3>
-          <SearchBar />
-        </div>
-        <div className="App-alert">
-          {nominations.length >= 5 && <Alert />}
-          <Confirm />
-        </div>
-        <div className="App-cards">
-          <ResultCard />
-          <NominationCard />
-        </div>
-      </main>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <header className="App-header">
+          <h1>The Shoppies</h1>
+        </header>
+        <main>
+          <div className="App-search">
+            <h3>Movie title:</h3>
+            <SearchBar />
+          </div>
+          <div className="App-alert">
+            {nominations.length >= 5 && <Alert />}
+            <Confirm />
+          </div>
+          <div className="App-cards">
+            <ResultCard />
+            <NominationCard />
+          </div>
+        </main>
+      </div>
+    </ThemeProvider>
   );
 };
 
