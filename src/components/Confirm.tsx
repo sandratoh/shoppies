6b7efd228e64b42ae5669d20927f916665d80a26
Sandra To <sandra.to.e@gmail.com>
2021-05-08 13:31:13 -0700

    Map out movie list from test data

--- src/components/Confirm.tsx
+++ src/components/Confirm.tsx
@@ -1,5 +1,5 @@
 // Libraries
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 
 // Component
 import Button from './Button';
@@ -11,11 +11,15 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 
+// Helpers
+import { MovieContext } from '../Provider/MovieDataProvider';
+
 // Stylesheet
 import './Confirm.scss';
 
 export const Confirm = () => {
-  const [open, setOpen] = useState(true);
+  // const { nominations } = useContext(MovieContext);
+  const [open, setOpen] = useState(false);
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -25,21 +29,53 @@ export const Confirm = () => {
     setOpen(false);
   };
 
+  const nominations = [
+    {
+      Title: 'Mean Girls',
+      Year: '2004',
+      imdbID: 'tt0377092',
+      Type: 'movie',
+      Poster:
+        'https://m.media-amazon.com/images/M/MV5BMjE1MDQ4MjI1OV5BMl5BanBnXkFtZTcwNzcwODAzMw@@._V1_SX300.jpg',
+    },
+    {
+      Title: 'The Avengers',
+      Year: '2012',
+      imdbID: 'tt0848228',
+      Type: 'movie',
+      Poster:
+        'https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg',
+    },
+    {
+      Title: 'The Matrix',
+      Year: '1999',
+      imdbID: 'tt0133093',
+      Type: 'movie',
+      Poster:
+        'https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg',
+    },
+  ];
+
+  const movieList = nominations.map(nomination => (
+    <li>{`${nomination.Title} (${nomination.Year})`}</li>
+  ));
+
   return (
-    <section className="confirma-dialog">
+    <section className="confirm-dialog">
       <Dialog
-        open={open}
+        open={true}
         onClose={handleClose}
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
+        fullWidth
+        maxWidth="sm"
       >
         <DialogTitle id="alert-dialog-title">
-          {"Use Google's location service?"}
+          {'Clear All from Nomination List?'}
         </DialogTitle>
         <DialogContent>
           <DialogContentText id="alert-dialog-description">
-            Let Google help apps determine location. This means sending
-            anonymous location data to Google, even when no apps are running.
+            {movieList}
           </DialogContentText>
         </DialogContent>
         <DialogActions>
