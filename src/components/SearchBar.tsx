9e52f0451a5e0c820c16fdfa5fec486c5c120b36
Sandra To <sandra.to.e@gmail.com>
2021-05-08 23:47:15 -0700

    Retrieve from api with debounced input value

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -23,27 +23,23 @@ export const SearchBar: React.FC<{}> = () => {
 
   const searchTerm = useDebounce(input, 200);
 
-  const onSearch = useCallback(term => setTerm(term), [searchTerm]);
-
   useEffect(() => {
     axios
       .get(
-        `https://www.omdbapi.com/?s=${input}&type=movie&apikey=${process.env.REACT_APP_OMDB_API_KEY}`
+        `https://www.omdbapi.com/?s=${term}&type=movie&apikey=${process.env.REACT_APP_OMDB_API_KEY}`
       )
       .then(res => {
         const movieResult: IMovie[] = res.data.Search;
         if (movieResult) {
           setSearchResult(movieResult);
-          setSearchKeyword(input);
-          // } else {
-          //   alert(
-          //     "Sorry, we couldn't find any movies matching your search. Try again with a different title."
-          //   );
+          setSearchKeyword(term);
         }
       });
-    // console.log(input);
-    // onSearch(input);
-  }, [input, onSearch]);
+  }, [term, setSearchResult, setSearchKeyword]);
+
+  useEffect(() => {
+    setTerm(searchTerm);
+  }, [searchTerm]);
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -55,23 +51,23 @@ export const SearchBar: React.FC<{}> = () => {
     document.getElementById('search-input')?.focus();
   };
 
-  const handleClick = () => {
-    return axios
-      .get(
-        `https://www.omdbapi.com/?s=${input}&type=movie&apikey=${process.env.REACT_APP_OMDB_API_KEY}`
-      )
-      .then(res => {
-        const movieResult: IMovie[] = res.data.Search;
-        if (movieResult) {
-          setSearchResult(movieResult);
-          setSearchKeyword(input);
-        } else {
-          alert(
-            "Sorry, we couldn't find any movies matching your search. Try again with a different title."
-          );
-        }
-      });
-  };
+  // const handleClick = () => {
+  //   return axios
+  //     .get(
+  //       `https://www.omdbapi.com/?s=${input}&type=movie&apikey=${process.env.REACT_APP_OMDB_API_KEY}`
+  //     )
+  //     .then(res => {
+  //       const movieResult: IMovie[] = res.data.Search;
+  //       if (movieResult) {
+  //         setSearchResult(movieResult);
+  //         setSearchKeyword(input);
+  //       } else {
+  //         alert(
+  //           "Sorry, we couldn't find any movies matching your search. Try again with a different title."
+  //         );
+  //       }
+  //     });
+  // };
 
   return (
     <form className="search-bar" onSubmit={handleSubmit}>
@@ -84,9 +80,10 @@ export const SearchBar: React.FC<{}> = () => {
         value={input}
         onChange={event => setInput(event.target.value)}
         onFocus={handleFocus}
-        // onSearch={onSearch}
       ></input>
-      <SearchButton onClick={handleClick} />
+      <SearchButton
+        onClick={() => console.log('will remove this button soon')}
+      />
     </form>
   );
 };
